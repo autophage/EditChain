dbe884b20ded9bbe00bb5bc06b61c6230d465e86
Eric Honour <autophage@gmail.com>
2023-04-17 16:37:02 -0400

    Initial commit

---
+++ EditChain/EditChain/EditChain.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+
+namespace EHonour.Utilities.EditChain
+{
+    [Serializable]
+    public abstract class EditChain<T> where T : ISerializable
+    {
+        private List<TimestampedObject<T>> versions = new List<TimestampedObject<T>>();
+
+        public void Update(T newVersion)
+        {
+            TimestampedObject<T> timestampedObject = new TimestampedObject<T>(newVersion);
+            versions.Add(timestampedObject);
+        }
+
+        public T GetCurrentVersion()
+        {
+            return versions.Last().Value;
+        }
+
+        public List<T> GetAllVersions()
+        {
+            return versions.Select(x => x.Value).ToList();
+        }
+
+        public IEnumerable<T> GetAllVersionsMatching(Func<T, bool> predicate)
+        {
+            return GetAllVersions().Where(predicate);
+        }
+    }
+}
---
+++ EditChain/EditChain/TimestampedObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace EHonour.Utilities.EditChain
+{
+    internal class TimestampedObject<T> where T : ISerializable
+    {
+        public TimestampedObject(T value){
+            this.Value = value;
+        }
+
+        public DateTime Timestamp { get; set; }
+        public T Value { get; set; }
+    }
+}
